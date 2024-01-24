0e6387171c96593be0205d7d6523d21faf8e981c
S-M-T09k <S.M.T.09k@gmail.com>
2024-01-25 06:25:41 +0630

    day-17 tic-tac-toe (working)

---
+++ one lesson per day/day-17 itc-tac-toe.js
@@ -0,0 +1,63 @@
+const cells = document.querySelectorAll('#day-17 #cellContainer .cell');
+const gameStatusDisplay = document.querySelector('#day-17 #gameStatus');
+const restartButton = document.querySelector('#day-17 #resetTicTacToe');
+
+// console.log(gameStatusDisplay);
+// console.log(restartButton);
+// console.log(cells);
+
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = 'X';
+let ticTacToeRunning = false;
+
+
+
+//*functions
+function initializeTicTacToe() {
+
+
+
+};
+
+function cellChecked() {
+
+
+
+};
+
+function updateCell(cell, index) {
+
+
+
+};
+
+function changePlayer() {
+
+
+
+};
+
+function checkTicTacToeWinner() {
+
+
+
+
+};
+
+function restartTicTacToe() {
+
+
+
+};
+
