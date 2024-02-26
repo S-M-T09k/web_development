7a1c0e19c0506af5dbba458004fe08651c8b2b08
S-M-T09k <S.M.T.09k@gmail.com>
2024-02-26 15:27:22 +0630

    (breakout) random blocks

--- other sites/games/breakout/breakout.js
+++ other sites/games/breakout/breakout.js
@@ -1,30 +1,150 @@
 const grid = document.querySelector('.grid');
 
+class Block {
 
-for (let i = 1; i < 21 * 3; i += 21) {
+  constructor(xPos, yPos, color,) {
+    this.xPos = xPos * 101;
+    this.yPos = yPos * 21;
+    this.color = color;
+  }
 
-  for (let j = 1; j < 101 * 10; j += 101) {
-    createAndAddBlock(j, i);
-  };
+  addToGrid() {
+    const block = document.createElement('div');
+    block.classList.add("block");
+    block.style.left = this.xPos + "px";
+    block.style.top = this.yPos + "px";
+    block.style.backgroundColor = this.color;
+    grid.appendChild(block);
+  }
 
 };
 
+const rainbow = [
+  '#FF0000',
+  '#FF7F00',
+  '#FFFF00',
+  '#00FF00',
+  '#0000FF',
+  '#4B0082',
+  '#8B00FF'
+];
 
-const user = document.createElement('div');
-const userStartPos = 460;
-let userCurrentPos = userStartPos;
-user.classList.add('user');
-user.style.bottom = "10px"
-user.style.left = userStartPos + 'px';
-grid.appendChild(user);
+const user = {
+  xPos: 460,
+  yPos: 10,
 
-function createAndAddBlock(blockX, blockY) {
+  addToGrid() {
+    const user = document.createElement('div');
+    user.classList.add("user");
+    user.style.left = this.xPos + "px";
+    user.style.bottom = this.yPos + "px";
+    user.style.backgroundColor = this.color;
+    grid.appendChild(user);
+  },
 
-  const block = document.createElement('div');
-  block.classList.add("block");
-  block.style.left = blockX + "px";
-  block.style.top = blockY + "px";
-  grid.appendChild(block);
+  currentPos: this.xPos,
 
-  //// console.log(blockX, blockY);
-}
+};
+
+const ball = {
+  xPos: 498,
+  yPos: 15,
+
+  addToGrid() {
+    const ball = document.createElement('div');
+    ball.classList.add("ball");
+    ball.style.left = this.xPos + "px";
+    ball.style.bottom = this.yPos + "px";
+    ball.style.backgroundColor = this.color;
+    grid.appendChild(ball);
+  },
+
+  currentPos: this.xPos,
+};
+
+
+let numberOfBlocks = 30;
+let rows = 4;
+const columns = 10;
+
+const blocks = [];
+
+for (let i = 0; i < numberOfBlocks; i++) {
+
+  let randomColor = rainbow[Math.floor(Math.random() * 7)];
+  let randomX = Math.floor(Math.random() * columns);
+  let randomY = Math.floor(Math.random() * rows);
+
+  // console.log(blocks.some(block => { block.xPos === randomX && block.yPos === randomY }));
+  //!divide because the assignment is multiplied
+  //!also array.some() doesn't work with {brackets}
+  while (blocks.some(block => block.xPos / 101 === randomX && block.yPos / 21 === randomY)) {
+    randomX = Math.floor(Math.random() * columns);
+    randomY = Math.floor(Math.random() * rows);
+  };
+
+  const block = new Block(randomX, randomY, randomColor);
+  blocks.push(block);
+
+};
+
+blocks.forEach(block => block.addToGrid());
+user.addToGrid();
+ball.addToGrid();
+
+
+//// blocks.forEach((block) => { block.addToGrid(); });
+// let placedBlocks = [];
+
+// for (let i = 0; i < numberOfBlocks; i++) {
+
+//   let randomColor = rainbow[Math.floor(Math.random() * 7)];
+//   let randomX = Math.floor(Math.random() * 10);
+//   let randomY = Math.floor(Math.random() * 4);
+
+//   for (const element of placedBlocks) {
+//     if ([randomX, randomY] == element) {
+//       break;
+//     }
+//   }
+
+//   const block = new Block(randomX, randomY, randomColor);
+//   block.addToGrid();
+//   placedBlocks.push([randomX, randomY]);
+
+// };
+
+
+
+// //document.createElement('div');
+// //const userStartPos = 460;
+// //let userCurrentPos = userStartPos;
+// //user.classList.add('user');
+// //user.style.bottom = "10px"
+// //user.style.left = userStartPos + 'px';
+// //grid.appendChild(user);
+
+// //function createAndAddBlock(blockX, blockY) {
+
+// //  const block = document.createElement('div');
+// //  block.classList.add("block");
+// //  block.style.left = blockX + "px";
+// //  block.style.top = blockY + "px";
+// //  grid.appendChild(block);
+
+//   //// console.log(blockX, blockY);
+// //}
+
+// const blocks = [
+//   new Block(0, 0, rainbow[Math.floor(Math.random() * 7)]),
+//   new Block(1, 0, rainbow[Math.floor(Math.random() * 7)]),
+//   new Block(2, 0, rainbow[Math.floor(Math.random() * 7)]),
+//   new Block(3, 0, rainbow[Math.floor(Math.random() * 7)]),
+//   new Block(4, 0, rainbow[Math.floor(Math.random() * 7)]),
+//   new Block(5, 0, rainbow[Math.floor(Math.random() * 7)]),
+//   new Block(6, 0, rainbow[Math.floor(Math.random() * 7)]),
+//   new Block(0, 1, rainbow[Math.floor(Math.random() * 7)]),
+//   new Block(1, 1, rainbow[Math.floor(Math.random() * 7)]),
+//   new Block(2, 1, rainbow[Math.floor(Math.random() * 7)]),
+//   new Block(3, 1, rainbow[Math.floor(Math.random() * 7)]),
+// ];
--- test.js
+++ test.js
@@ -13,3 +13,52 @@
 //   };
 
 // };
+
+
+
+class Block {
+
+  constructor(xPos, yPos, color,) {
+    this.xPos = xPos * 101;
+    this.yPos = yPos * 21;
+    this.color = color;
+  }
+
+  addToGrid() {
+    const block = document.createElement('div');
+    block.classList.add("block");
+    block.style.left = this.xPos + "px";
+    block.style.top = this.yPos + "px";
+    block.style.backgroundColor = this.color;
+    grid.appendChild(block);
+  }
+
+};
+
+let rainbow = "#ffffff";
+const blocks = [];
+
+for (let i = 0; i < 100; i++) {
+
+  let randomColor = rainbow;
+  let randomX = Math.floor(Math.random() * 10);
+  let randomY = Math.floor(Math.random() * 4);
+
+  while (blocks.some((block) => {block.xPos === randomX && block.yPos === randomY})) {
+    randomX = Math.floor(Math.random() * 10);
+    randomY = Math.floor(Math.random() * 4);
+  }
+
+  const block = new Block(randomX, randomY, randomColor);
+  blocks.push(block);
+}
+
+// blocks.forEach((block) => {
+//   console.log("x: " + block.xPos + ", y: " + block.yPos);
+// });
+
+console.log(
+  blocks.some((block) => {
+    return block.xPos === 0 && block.yPos === 0;
+  })
+);
